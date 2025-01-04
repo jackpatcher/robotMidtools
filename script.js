3200a0ed7e3f8a6b542e302c97c0e607a61348a6
jackpatcher <50197512+jackpatcher@users.noreply.github.com>
2025-01-04 14:49:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js')
+  navigator.serviceWorker.register('service_worker.js')
     .then(() => console.log('Service Worker registered'))
     .catch(err => console.error('Service Worker registration failed:', err));
 }
